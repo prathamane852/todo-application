5bc025a7aa30263a32e27b33201e42daf9b1c903
Prathamesh <prathameshmane855@gmail.com>
2026-05-07 15:02:26 +0530

    todo Added using js

--- index.js
+++ index.js
@@ -1,4 +1,73 @@
 // adding a events inside DOM
+
+let todoDataSection = document.getElementById('todo-data');
+function addtodo(todoData) {
+    let rowDiv= document.createElement('div')
+    let todoItem= document.createElement('div')
+    let todoNumber = document.createElement('div')
+    let todoDetail = document.createElement('div')
+    let todoStatus = document.createElement('div')
+    let todoActions = document.createElement('div')
+    let deleteButton = document.createElement('button')
+    let finishedButton = document.createElement('button')
+    let hr=  document.createElement('hr');
+
+    todoNumber.textContent = "1";
+    todoDetail.textContent = todoData; // sets the todo text sent from input element
+    todoStatus.textContent = "In Progress"
+    deleteButton.textContent = "Delete"
+    finishedButton.textContent = "Finished"
+
+    todoActions.appendChild(deleteButton);
+    todoActions.appendChild(finishedButton);
+
+
+    todoItem.appendChild(todoNumber);
+    todoItem.appendChild(todoDetail);
+    todoItem.appendChild(todoStatus);
+    todoItem.appendChild(todoActions);
+
+    rowDiv.appendChild(todoItem);
+    rowDiv.appendChild(hr);
+
+    todoDataSection.appendChild(rowDiv);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Events are happen in system, system tell you about it and your code will react to them ( A Specific action or interaction)
 
 // example : User clicks the button on webpage , Browser trigger the event and you can add some handler (A Custom code excuted) for it
